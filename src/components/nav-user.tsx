673a5dd7cc15c8e1bfc67596dfbc8fc2ea22cc19
code-paolo <psibs123@gmail.com>
2024-11-01 21:21:07 +0800

    Fixed the Imports

--- src/components/nav-user.tsx
+++ src/components/nav-user.tsx
@@ -1,12 +1,5 @@
-import {
-  BadgeCheck,
-  Bell,
-  ChevronsUpDown,
-  CreditCard,
-  LogOut,
-  Sparkles,
-} from "lucide-react";
-import { Avatar, AvatarFallback, AvatarImage } from "../components/ui/avatar";
+import { BadgeCheck, Bell, ChevronsUpDown, LogOut } from "lucide-react";
+import { Avatar, AvatarFallback } from "../components/ui/avatar";
 import {
   DropdownMenu,
   DropdownMenuContent,
